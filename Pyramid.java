5abe99c322633b8121e135437cfa8b9e8927ebf9
Quaintbrake5 <144044983+Quaintbrake5@users.noreply.github.com>
2024-07-21 20:01:27 +0100

    Update Pyramid.java

--- Pyramid.java
+++ Pyramid.java
@@ -4,10 +4,13 @@
 public class Pyramid {
     public static void main(String[] args) {
         int n = 8;
-
-        for (int i = n; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-                System.out.print("*");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j < n - i - 1) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
             }
             System.out.println();
         }
